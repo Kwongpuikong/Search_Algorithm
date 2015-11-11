d8a53de364e8be719148f9ef3111fd57723ad7ce
kwong <kenny-k@foxmail.com>
2015-11-11 21:06:53 +0800

    完成二叉查找树

--- Serch/Search.cpp
+++ Serch/Search.cpp
@@ -2,6 +2,9 @@
 /*Author: Loapui*/
 /*2015.11.10*/
 
+/*完成二叉查找树*/
+/*2015.11.11*/
+
 #include "Search.h"
 
 void SuccessMessage(int key, int i){
@@ -110,4 +113,227 @@ int Interpolation_Search(int L[], int low, int high, int key){
 	}
 	ErrorMessage(key);
 	return -1;
+}
+
+int Fibonacci(int x){
+
+	if (x == 1 || x == 2){
+	
+		return 1;
+	}
+	else{
+	
+		return Fibonacci(x - 2) + Fibonacci(x - 1);
+	}
+}
+
+int Fibonacci_Search(int L[], int low, int high, int key){
+
+	int n = high - low + 1;
+	int k = 1;
+	while (Fibonacci(k)-1 < n){
+	
+		k++;
+	}
+	// 补全不满的位置
+	for (int i = high + 1; i <= Fibonacci(k)-1; i++){
+	
+		L[i] = L[high];
+	}
+
+	int mid;
+	while (low <= high){
+	
+		mid = low + Fibonacci(k - 1) - 1;
+		if (L[mid] == key){
+
+			// std::cout << "mid = " << mid << std::endl;
+		
+			if (mid <= high){
+			
+				SuccessMessage(key, mid);
+				return mid;
+			}
+			else{
+			
+				SuccessMessage(key, high);
+				return high;
+			}
+
+		}
+		else if (L[mid] > key){
+		
+			high = mid - 1;
+			k -= 1;
+		}
+		else if (L[mid] < key){
+		
+			low = mid + 1;
+			k -= 2;
+		}
+	}
+	ErrorMessage(key);
+	return -1;
+
+}
+
+bool BST_Search(BiTree T, int key, BiTree parent, BiTree* res){
+
+	if (!T){
+
+		*res = parent;
+		return false;
+	}
+	else if (T->data == key){
+	
+		*res = T;
+		return true;
+	}
+	else if (T->data < key){
+
+		BST_Search(T->rchild, key, T, res);
+	}
+	else{
+	
+		BST_Search(T->lchild, key, T, res);
+	}
+
+}
+
+bool BST_Insert(BiTree* T, int key){
+
+	BiTree lst;
+	if (!BST_Search(*T, key, NULL, &lst)){
+
+		BiTree p = (BiTree)malloc(sizeof(BiTNode));
+		if (!p){
+			
+			std::cout << "Out of Memory!!!" << std::endl;
+			return false;
+		}
+		else{
+
+			p->data = key;
+			p->lchild = NULL;
+			p->rchild = NULL;
+		}
+
+		if (!lst){
+		
+			*T = p;
+		}
+		else if (lst->data < key)
+				lst->rchild = p;
+		else
+				lst->lchild = p;
+		return true;
+	}
+	else
+		return false;
+}
+
+bool BST_Construct(int L[], int length, BiTree *T){
+
+	for (int i = 1; i <= length; i++){
+	
+		if (!BST_Insert(T, L[i]))
+			return false;
+	}
+	return true;
+}
+
+void SearchBST(BiTree* T, int key){
+
+	BiTree lst;
+	bool flag = BST_Search(*T, key, NULL, &lst);
+	if (!flag){
+	
+		ErrorMessage(key);
+		BST_Insert(T, key);
+		std::cout << "Finish Inserting." << std::endl;
+	}
+	else{
+	
+		std::cout << "p->data = " << lst->data << std::endl;
+	}
+}
+
+bool BST_Delete(BiTree *T, int key){
+
+	if (!(*T))
+		return false;
+	else if ((*T)->data == key){
+	
+		return Delete(T);
+	}
+	else if ((*T)->data < key){
+
+		return BST_Delete(&(*T)->rchild, key);
+	}
+	else
+		return BST_Delete(&(*T)->lchild, key);
+
+}
+
+bool Delete(BiTree *p){
+
+	BiTree ptr, s;
+
+	if (!(*p)->lchild){
+
+		ptr = (*p);
+		*p = (*p)->rchild;
+		free(ptr);
+	}
+	else if (!(*p)->lchild){
+	
+		ptr = *p;
+		*p = (*p)->rchild;
+		free(ptr);
+	}
+	else{
+		
+		ptr = *p;
+		s = ptr->lchild;
+		while (s->rchild){
+
+			ptr = s;
+			s = s->rchild;
+		}
+		(*p)->data = s->data;
+		if (ptr != (*p))
+			ptr->rchild = s->lchild;
+		else
+			ptr->lchild = s->lchild;
+
+		// Important.
+		free(s);
+	}
+	return true;
+
+}
+
+int BST_Max(BiTree T){
+
+	BiTree p = NULL;
+	if (T)
+		p = T->rchild;
+	while (p->rchild){
+	
+		p = p->rchild;
+	}
+	return p->data;
+
+}
+
+int BST_Min(BiTree T){
+
+	BiTree p = NULL;
+	if (T)
+		p = T->lchild;
+	while (p->lchild){
+	
+		p = p->lchild;
+	}
+	return p->data;
 }
--- Serch/Search.h
+++ Serch/Search.h
@@ -2,15 +2,42 @@
 /*Author: Loapui*/
 /*2015.11.10*/
 
+/*完成二叉查找树*/
+/*2015.11.11*/
+
 #ifndef SEARCH_H
 #define SEARCH_H
 
 #include <iostream>
 
+typedef struct BiTNode{
+
+	int data;
+	BiTNode *lchild, *rchild;
+}BiTnode, *BiTree;
+
 int Sequential_Search(int L[], int length, int key);
 int Binary_Search(int L[], int low, int high, int key, bool recursive = true);
 int Interpolation_Search(int L[], int low, int high, int key);
+int Fibonacci_Search(int L[], int low, int high, int key);
 
+/*利用二叉查找树查找关键字*/
+bool BST_Search(BiTree T, int key, BiTree parent, BiTree* res);
+bool BST_Insert(BiTree* T, int key);
+void SearchBST(BiTree* T, int key);
+
+/*删除二叉查找树结点*/
+bool BST_Delete(BiTree* T, int key);
+bool Delete(BiTree* T);
+
+/*构建二叉查找树*/
+bool BST_Construct(int L[],int length, BiTree *T);
+
+/*利用二叉查找树得最大最小值*/
+int BST_Max(BiTree T);
+int BST_Min(BiTree T);
+
+int Fibonacci(int x);
 void ErrorMessage(int key);
 void SuccessMessage(int key, int i);
 
